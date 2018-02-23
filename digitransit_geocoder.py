a6a72cba736d896f54fb4b1430787f2d3a2c7d71
Erno Mäkinen <ernoma@gmail.com>
2018-02-24 00:04:52 +0200

    Add signal handler skeleton for the address search button click event

--- digitransit_geocoder.py
+++ digitransit_geocoder.py
@@ -204,7 +204,7 @@ class DigitransitGeocoder:
             #    removed on close (see self.onClosePlugin method)
             if self.dockwidget == None:
                 # Create the dockwidget (after translation) and keep reference
-                self.dockwidget = DigitransitGeocoderDockWidget()
+                self.dockwidget = DigitransitGeocoderDockWidget(self.iface)
 
             # connect to provide cleanup on closing of dockwidget
             self.dockwidget.closingPlugin.connect(self.onClosePlugin)
--- digitransit_geocoder_dockwidget.py
+++ digitransit_geocoder_dockwidget.py
@@ -16,7 +16,11 @@
 import os
 
 from PyQt5 import QtGui, QtWidgets, uic
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import pyqtSignal, QMetaObject, pyqtSlot
+#from PyQt5.QtWidgets import QMessageBox
+from qgis.core import Qgis, QgsMessageLog
+from PyQt5 import  QtNetwork
+from PyQt5.QtCore import QUrl
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'digitransit_geocoder_dockwidget_base.ui'))
@@ -26,9 +30,12 @@ class DigitransitGeocoderDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
 
     closingPlugin = pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, iface, parent=None):
         """Constructor."""
         super(DigitransitGeocoderDockWidget, self).__init__(parent)
+
+        self.iface = iface
+
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots - see
@@ -36,6 +43,30 @@ class DigitransitGeocoderDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
+        # Note: no need to call the QMetaObject.connectSlotsByName(self)
+        #QMetaObject.connectSlotsByName(self)
+
+        # Note this is not because we have the auto connection
+        #self.pushButtonSearch.clicked.connect(self.handleSearch)
+
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
+
+    #def handleSearch(self):
+    #    QgsMessageLog.logMessage('This is an important message',
+    #                             'Important Information',
+    #                             Qgis.Info)
+
+    @pyqtSlot() # Without this the signal is handled twice, see https://stackoverflow.com/questions/14311578/event-signal-is-emmitted-twice-every-time
+    def on_pushButtonSearch_clicked(self):
+        #QMessageBox.information(None,
+        #                        'Important Information',
+        #                        'This is an important message')
+
+        QgsMessageLog.logMessage(self.lineEditAddress.text(),
+                                 'Search term',
+                                 Qgis.Info)
+
+    def geocode(self, search_text):
+        baseURL = "http://api.digitransit.fi/geocoding/v1/search"
