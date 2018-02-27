7d054175952e70c8ee69716951b90a4607bd8356
Erno Mäkinen <ernoma@gmail.com>
2018-02-27 17:24:10 +0200

    Translate to Finnish

---
+++ i18n/DigitransitGeocoder_fi.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi_FI" sourcelanguage="en_US">
+<context>
+    <name>DigitransitGeocoder</name>
+    <message>
+        <location filename="../digitransit_geocoder.py" line="186"/>
+        <source>&amp;Digitransit.fi Geocoder</source>
+        <translation>&amp;Digitransit.fi-geokoodaus</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder.py" line="160"/>
+        <source>Geocode a Finnish address</source>
+        <translation>Geokoodaa suomalainen osoite</translation>
+    </message>
+</context>
+<context>
+    <name>DigitransitGeocoderDockWidgetBase</name>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="14"/>
+        <source>Geocoding based on Digitransit.fi</source>
+        <translation>Digitransit.fi-rajapintaan perustuva geokoodaus</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="23"/>
+        <source>Address:</source>
+        <translation>Osoite:</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="30"/>
+        <source>Search</source>
+        <translation>Hae</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="43"/>
+        <source>Search parameters</source>
+        <translation>Hakuparametrit</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="51"/>
+        <source>Maximum number of addresses to list</source>
+        <translation>Haettavien osoitteiden maksimimäärä</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="73"/>
+        <source>Search only from the visible map area</source>
+        <translation>Hae ainoastaan karttanäkymän alueelta</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="80"/>
+        <source>Data sources</source>
+        <translation>Tietolähteet</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="86"/>
+        <source>OpenAddresses (VRK)</source>
+        <translation>OpenAddress (VRK)</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="96"/>
+        <source>OpenStreetMap</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="106"/>
+        <source>NLS.fi</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="119"/>
+        <source>Types of locations</source>
+        <translation>Sijaintityypit</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="125"/>
+        <source>Addresses</source>
+        <translation>Osoitteet</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="135"/>
+        <source>Places and venues</source>
+        <translation>Paikat ja tapahtumapaikat</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="145"/>
+        <source>Streets</source>
+        <translation>Kadut</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="161"/>
+        <source>Showing search results on the map</source>
+        <translation>Hakutulosten lisääminen kartalle</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="167"/>
+        <source>Show all search results on the map</source>
+        <translation>Näytä kaikki hakutulokset</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="180"/>
+        <source>Show the selected result list item on the map</source>
+        <translation>Näytä hakutuloslistalta valittu kohde</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/translate.pro
@@ -0,0 +1,7 @@
+
+FORMS = ../digitransit_geocoder_dockwidget_base.ui
+
+SOURCES = ../digitransit_geocoder.py \
+    ../digitransit_geocoder_dockwidget.py
+
+TRANSLATIONS = DigitransitGeocoder_fi.ts
