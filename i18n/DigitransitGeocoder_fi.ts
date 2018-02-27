b824d3190df67978c64cc87a191bdd29d4d12f64
Erno Mäkinen <ernoma@gmail.com>
2018-02-27 18:17:07 +0200

    Update plugin description and data attributions

--- i18n/DigitransitGeocoder_fi.ts
+++ i18n/DigitransitGeocoder_fi.ts
@@ -22,84 +22,94 @@
         <translation>Digitransit.fi-rajapintaan perustuva geokoodaus</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="23"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="42"/>
         <source>Address:</source>
         <translation>Osoite:</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="30"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="49"/>
         <source>Search</source>
         <translation>Hae</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="43"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="62"/>
         <source>Search parameters</source>
         <translation>Hakuparametrit</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="51"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="70"/>
         <source>Maximum number of addresses to list</source>
         <translation>Haettavien osoitteiden maksimimäärä</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="73"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="92"/>
         <source>Search only from the visible map area</source>
         <translation>Hae ainoastaan karttanäkymän alueelta</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="80"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="99"/>
         <source>Data sources</source>
         <translation>Tietolähteet</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="86"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="105"/>
         <source>OpenAddresses (VRK)</source>
         <translation>OpenAddress (VRK)</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="96"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="115"/>
         <source>OpenStreetMap</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="106"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="125"/>
         <source>NLS.fi</source>
-        <translation></translation>
+        <translation>Maanmittauslaitoksen maastotietokanta</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="119"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="138"/>
         <source>Types of locations</source>
         <translation>Sijaintityypit</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="125"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="144"/>
         <source>Addresses</source>
         <translation>Osoitteet</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="135"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="154"/>
         <source>Places and venues</source>
         <translation>Paikat ja tapahtumapaikat</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="145"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="164"/>
         <source>Streets</source>
         <translation>Kadut</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="161"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="180"/>
         <source>Showing search results on the map</source>
         <translation>Hakutulosten lisääminen kartalle</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="167"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="186"/>
         <source>Show all search results on the map</source>
         <translation>Näytä kaikki hakutulokset</translation>
     </message>
     <message>
-        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="180"/>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="199"/>
         <source>Show the selected result list item on the map</source>
         <translation>Näytä hakutuloslistalta valittu kohde</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="21"/>
+        <source>Attributions</source>
+        <translation>Aineistolähteet</translation>
+    </message>
+    <message>
+        <location filename="../digitransit_geocoder_dockwidget_base.ui" line="27"/>
+        <source>Please, note that the data is from the &lt;a href=&quot;http://www.maanmittauslaitos.fi/en/maps-and-spatial-data/expert-users/topographic-data-and-how-acquire-it&quot;&gt;National Land Survey of Finland Topographic Database&lt;/a&gt;, &lt;a href=&quot;https://www.avoindata.fi/data/fi/dataset/rakennusten-osoitetiedot-koko-suomi&quot;&gt;Population Register Centre of Finland&lt;/a&gt; and &lt;a href=&quot;https://www.openstreetmap.org&quot;&gt;OpenStreetMap&lt;/a&gt;. The National Land Survey (NLS.fi) and the Population Register Centre (via OpenAddresses) data are licensed under &lt;a href=&quot;https://creativecommons.org/licenses/by/4.0/&quot;&gt;CC BY 4.0 license&lt;/a&gt;. The OpenStreetMap data is licensed under &lt;a href=&quot;https://opendatacommons.org/licenses/odbl/&quot;&gt;ODbL license&lt;/a&gt;.</source>
+        <translation>Aineistolähteinä ovat &lt;a href=&quot;http://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/asiantuntevalle-kayttajalle/maastotiedot-ja-niiden-hankinta&quot;&gt;Maanmittauslaitoksen Maastotietokanta&lt;/a&gt;, &lt;a href=&quot;https://www.avoindata.fi/data/fi/dataset/rakennusten-osoitetiedot-koko-suomi&quot;&gt;Väestörekisterikeskus&lt;/a&gt; ja &lt;a href=&quot;https://www.openstreetmap.org&quot;&gt;OpenStreetMap&lt;/a&gt;. Maanmittauslaitoksen maastotieokanta ja Väestörekisterikeskuksen aineisto on lisensoitu &lt;a href=&quot;https://creativecommons.org/licenses/by/4.0/&quot;&gt;Creative Commons Nimeä 4.0 Kansainvälinen -lisenssillä&lt;/a&gt;. OpenStreetMap-aineisto on lisensoitu &lt;a href=&quot;https://opendatacommons.org/licenses/odbl/&quot;&gt;ODbL-lisenssillä&lt;/a&gt;.</translation>
     </message>
 </context>
 </TS>
